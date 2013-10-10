bd39e23c167c78b28d65918e20ca1f0db8c9a68f
Pranav K <prkrishn@hotmail.com>
2013-10-10 14:15:10 -0700

    Minor tweaks

--- src/NuGetGallery/Scripts/nugetgallery.js
+++ src/NuGetGallery/Scripts/nugetgallery.js
@@ -53,18 +53,6 @@
         $input.delegate('', 'focus', popit(true));
     }
 
-    function checkServiceStatus() {
-        $.get(app.root + 'api/v2/service-alert?cachebust=' + new Date().getTime())
-            .done(function (data) {
-                if (typeof data === 'string' && data.length > 0) {
-                    $('#service-alert').html(data).slideDown('fast');
-                }
-                else {
-                    $('#service-alert').slideUp('fast').html();
-                }
-            }) // If this fails, just silently show no status.
-    }
-
     $(function () {
         // Export an object with global config data
         var app = $(document.documentElement).data();
